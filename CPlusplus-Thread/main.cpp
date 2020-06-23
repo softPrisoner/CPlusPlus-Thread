bb662b11be0a289c5465d1d95ae40deb84bf0ebf
tony <wabslygzj@163.com>
2020-06-23 14:03:50 +0800

    temporary variable in multipart thread environment

--- CPlusplus-Thread/main.cpp
+++ CPlusplus-Thread/main.cpp
@@ -7,10 +7,29 @@
 using namespace std;
 
 
+class A {
+	int& m_i;
+public:
+	A(int& i) :m_i(i) {
+		cout << "A 构造函数被执行" << endl;
+		cout << "m_i的值:" << m_i << endl;
+	};
+	A(const A& a) :m_i(a.m_i) {
+		cout << "A 拷贝构造函数被执行" << endl;
+	};
+	~A() {
+		cout << "A 析构函数被执行" << endl;
+	};
+};
 
-void myprint() {
+//void myprint(const int& i, char* pmybuf) {
+//void myprint(const int& i, const string& pmybuf) {
+void myprint(const int i, const A& pmybuf) {
 	cout << "我的线程开始执行\n" << endl;
+	cout << i << endl;
+	cout << &pmybuf << endl;
 	cout << "我的线程执行完毕\n" << endl;
+	return;
 }
 
 class TA {
@@ -60,11 +79,27 @@ int main() {
 	//mythread2.detach();
 	//###################################使用lambda表达式实现线程调用执行
 	//匿名函数
-	auto mylambda = [] {
-		cout << "我的线程3开始执行了" << endl;
-	};
-	thread mylambdathread(mylambda);
-	mylambdathread.join();
-	cout << "主线程执行完毕\n" << endl;
-	return 0;
+	//auto mylambda = [] {
+	//	cout << "我的线程3开始执行了" << endl;
+	//};
+	//thread mylambdathread(mylambda);
+	//mylambdathread.join();
+	//cout << "主线程执行完毕\n" << endl;
+	//return 0;
+
+//#############################################detach陷阱
+	//传递临时对象作为线程参数
+	//（1.1）要避免的陷阱
+	//int mvar = 1;
+	//int mvr = 1;
+	//int& mvary = mvar;
+	//char mybuf[] = "this is a test!";
+	//thread  mytobj(myprint, mvar, string(mybuf));//mybuf在什么时候转换成string
+	////mytobj.join();//子线程和主线程同时运行
+	//mytobj.detach();//子线程和主线程分别执行
+	//cout << "I Love China!" << endl;
+	//#####################################模拟过程
+	int mvar = 1;
+	int mysecondpar = 12;
+	thread  mytobj(myprint, mvar, A(mysecondpar));//mybuf在什么时候转换成string
 }
